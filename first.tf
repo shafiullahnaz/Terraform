3e21b0f3305b7b99c72af459a1ab617aa14e21a6
Shafi Ullah <119392344+shafiullahnaz@users.noreply.github.com>
2024-08-03 12:31:23 +0500

    Create first.tf
    this is the first we are going to create more and more so start now!

---
+++ first.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0ba9883b710b05ac6"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0e64537f1061da1c5"
+    key_name = "aws-persnol"
+    
+}
