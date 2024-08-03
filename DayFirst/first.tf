128ddbc330772582695e4d79712758105beab44c
Shafi Ullah <119392344+shafiullahnaz@users.noreply.github.com>
2024-08-03 13:59:49 +0500

    Update first.tf

--- DayFirst/first.tf
+++ DayFirst/first.tf
@@ -5,6 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0ba9883b710b05ac6"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
-    subnet_id = "subnet-0e64537f1061da1c5"
-    key_name = "aws-persnol"
+    subnet_id = "subnet-0e64537f1061da1c5" #Copy the image that you want to create 
+    key_name = "aws-persnol"  #also create your own key and mention at here.
 }
